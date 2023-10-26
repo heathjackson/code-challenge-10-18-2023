87b437bbe5555448ec765e1b97204b552f3a93f2
Heather <80366350+heathjackson@users.noreply.github.com>
2023-10-25 22:09:53 -0400

    challenge 1 done and tests complete

--- src/challenge01.js
+++ src/challenge01.js
@@ -1,9 +1,19 @@
 /**
  * Write a function (WITHOUT USING STRING methods) to reverse the order of a string.
- *
- * @param {string} value
- * @returns
+ * 
+ * @param {string} value 
+ * @returns 
  */
-const reverseIt = (value) => {};
+export const reverseIt = (value) => {
+  let newString = "" 
 
-export default reverseIt;
+  for(let i = value.length - 1; i >= 0; i--) {
+    newString += value[i]
+  }
+  return newString 
+}
+
+export const reverseArray = (arr) => arr.map(str => reverseIt(str))
+
+
+console.log(reverseArray(['JavaScript', 'HTML', 'XML', 'Java', 'Python', 'Perl', 'Go', 'C++']))
--- test/challenge01.test.js
+++ test/challenge01.test.js
@@ -1,21 +1,16 @@
-import reverseIt from "../src/challenge01";
+import { reverseIt, reverseArray } from "../src/challenge01"
 
-describe("Challenge 01", () => {
-  const t1 = "JavaScript";
-  const t2 = [
-    "JavaScript",
-    "HTML",
-    "XML",
-    "Java",
-    "Python",
-    "Perl",
-    "Go",
-    "C++",
-  ];
+describe('Challenge 01', () => {
+  const t1 = 'JavaScript'
+  const t2 = ['JavaScript', 'HTML', 'XML', 'Java', 'Python', 'Perl', 'Go', 'C++']
 
-  test("Reverse a single string", () => {});
+  test('Reverse a single string', () => {
+    // Reverse the single string,
+    expect(reverseIt(t1)).toEqual('tpircSavaJ')
+  })
 
-  test("Reverse all strings in an array", () => {
+  test('Reverse all strings in an array', () => {
     // Reverse all string in an array
-  });
-});
+    expect(reverseArray(t2)).toEqual([ 'tpircSavaJ', 'LMTH', 'LMX', 'avaJ', 'nohtyP', 'lreP', 'oG', '++C' ])
+  })
+})
